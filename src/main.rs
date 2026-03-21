0adadfbc0e1e03f0337e93e81579ce7c4245f829
vladyslav.lutchyn <v-hive@Vladyslavs-MacBook-Pro.local>
2026-03-21 19:11:43 -0400

    Add detailed logging for file reading, lexing, parsing, and evaluation. For near future separation of modules

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,17 @@ pub(crate) mod ast;
 pub mod JsValue;
 
 fn main() {
+    println!("[Swiftly-Info]: Reading file...");
     let mut f = File::open("./examples/example.js").unwrap();
     let mut buffer = String::new();
     f.read_to_string(&mut buffer).unwrap();
+    println!("[Swiftly-Info]: File read successfully!");
+
+    // Lexer
     let mut lexer: Lexer = Lexer::new(&buffer);
     let mut tokens = Vec::new();
 
-    println!("[Swiftly-Info]: Lexing tokens...");
+    println!("[Swiftly-Info]: Generating tokens...");
     loop {
         let token: Token = lexer.next_token();
         println!("{:?}", token);
@@ -30,13 +34,20 @@ fn main() {
             break;
         }
     }
+    println!("[Swiftly-Info]: Tokens generated successfully!");
+    // ---
 
+
+    // Parser
     println!("[Swiftly-Info]: Parsing program...");
     let mut parser = Parser::new(tokens);
     let program = parser.parse_program();
-    println!("{:#?}", program.body);
+    println!("[Swiftly-Info]: AST: {:#?}", program.body);
+
+    // ---
 
     println!("[Swiftly-Info]: Evaluating...");
     let mut interpreter = Interpreter::new();
     interpreter.execute(program.body);
+    println!("[Swiftly-Info]: Done!");
 }
